c85f77eef33650a611c28c59657c0f2160afff60
Kaif Ali <117533671+Codewithkaif0786@users.noreply.github.com>
2025-05-09 17:12:07 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 138844;
+let accountEmail ="kaif@gmail.com";
+var accountPass = "13834";
+newCity = "patna";
+let accountState;
+
+/* Prefer not use var because of issue in scope block and function scope */
+
+accountEmail = "hera@gmail.com";
+accountPass = "0787";
+newCity = "gopalganj";
+
+
+// accountId = 2;
+//console.log(accountId);
+console.table([accountId, accountEmail, accountPass, newCity,accountState]);
